569568cef64e7ea01d6c617ddeec2497d51c2cd6
Andrew Branch <andrew@wheream.io>
2018-10-21 14:52:32 -0700

    Initial Commit

---
+++ GlanceCalendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GlanceCalendar
+//
+//  Created by Andrew Branch on 10/21/18.
+//  Copyright © 2018 Wheream.io. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
