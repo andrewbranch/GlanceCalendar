adcf686c3c08968cb04c09a1391de5f9e4e77aed
Andrew Branch <andrew@wheream.io>
2018-10-28 14:22:57 -0700

    Allow vibrancy in calendar header view

---
+++ GlanceCalendar/CalendarHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarHeaderView.swift
+//  GlanceCalendar
+//
+//  Created by Andrew Branch on 10/28/18.
+//  Copyright © 2018 Wheream.io. All rights reserved.
+//
+
+import Cocoa
+
+class CalendarHeaderView: NSView {
+    override var allowsVibrancy: Bool {
+        get {
+            return true
+        }
+    }
+}
